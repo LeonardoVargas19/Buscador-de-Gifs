19ae8840756937bb25b939e8d10682c3968a2e80
Leonardo Vargas Olvera <124732244+LeonardoVargas19@users.noreply.github.com>
2023-02-21 19:45:45 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { ShareModule } from './share/share.module';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ShareModule,
+    GifsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
